d3e80fab58919b77809da5ba0c445eca7e0703c2
Luigi <hpcarg@gmail.com>
2020-12-08 09:52:06 -0300

    little update

--- src/app.py
+++ src/app.py
@@ -2,30 +2,40 @@ import pymongo
 from flask import Flask, request, jsonify, Response
 from flask_pymongo import PyMongo
 from bson import json_util
+from datetime import datetime
 
 app = Flask(__name__)
-app.config['MONGO_URI']='mongodb+srv://luigi:Luigi2020@cluster0.o2pfg.mongodb.net/db?retryWrites=true&w=majority'
+app.config['MONGO_URI'] = 'mongodb+srv://luigi:Luigi2020@cluster0.o2pfg.mongodb.net/db?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
+
 @app.route('/')
 def index():
-    return "<spam>Welcome to our server !!</spam>"
+    date_now = datetime.now()
+    dt_string = date_now.strftime("%d/%m/%Y %H:%M:%S")
+
+    return "<h3>🙇‍♂️ " + dt_string + "</h3>"
+
 
 @app.route('/values', methods=['GET'])
 def get_values():
-    values = mongo.db.values.find_one({}, {'_id': 0, 'creationDate': 0}, sort=[( '_id', pymongo.DESCENDING )])
+    values = mongo.db.values.find_one({}, {'_id': 0, 'creationDate': 0}, sort=[
+                                      ('_id', pymongo.DESCENDING)])
     response = json_util.dumps(values)
-    
+
     return Response(response, mimetype="application/json")
+
 
 @app.route('/types', methods=['GET'])
 def get_types():
     types = mongo.db.types.find({})
     response = json_util.dumps(types)
-    
+
     return Response(response, mimetype="application/json")
 
+
 # esto no corre con el debugger
+# cambiar a 0.0.0.0 Debug=True para local
 if __name__ == "__main__":
     app.run(threaded=True, port=5000)
